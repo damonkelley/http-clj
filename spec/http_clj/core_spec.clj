07ab400bc4be29af60eed9177daff73910d530d8
Damon Kelley <damon.kelley@gmail.com>
2016-08-04 14:00:21 -0500

    Add the echo-loop

--- spec/http_clj/core_spec.clj
+++ spec/http_clj/core_spec.clj
@@ -1,3 +1,13 @@
 (ns http-clj.core-spec
-  (:require [speclj.core :refer :all]
-            [http-clj.core :refer :all]))
+ (:require [speclj.core :refer :all]
+       [http-clj.core :refer :all]
+       [http-clj.connection :refer [new-connection]]
+       [http-clj.helpers :refer [mock-socket]])
+ (:import java.io.ByteArrayOutputStream))
+
+
+(describe "An echo loop"
+  (with output (ByteArrayOutputStream.))
+  (it "echos until it receives bye."
+    (echo-loop (new-connection (mock-socket "foo\nbye." @output)))
+    (should= "foo\nGoodbye\n" (.toString @output))))
--- src/http_clj/core.clj
+++ src/http_clj/core.clj
@@ -1,1 +1,15 @@
-(ns http-clj.core)
+(ns http-clj.core
+ (:require [http-clj.connection :as connection])
+ (:import java.net.ServerSocket))
+
+
+(defn- echo [text conn]
+  (connection/write conn text)
+  conn)
+
+(defn echo-loop [conn]
+  (loop [conn conn]
+   (let [text (connection/readline conn)]
+     (if (= "bye." text)
+       (echo "Goodbye" conn)
+       (recur (echo text conn))))))
