cd411dd83dfc7eee5dc77b068ea59b5450eaff18
Damon Kelley <damon.kelley@gmail.com>
2016-08-16 14:33:38 -0500

    Fix indentation

--- src/http_clj/response.clj
+++ src/http_clj/response.clj
@@ -18,10 +18,10 @@
   (let [version "HTTP/1.1"
         status status
         reason (reason-for status)]
-  (string/join " " [version status reason])))
+    (string/join " " [version status reason])))
 
 (defn compose [resp]
   (let [{status :status body :body} resp]
-  (assoc resp :message (str (compose-status-line status) "\r\n"
-                            "\r\n"
-                            body))))
+    (assoc resp :message (str (compose-status-line status) "\r\n"
+                              "\r\n"
+                              body))))
