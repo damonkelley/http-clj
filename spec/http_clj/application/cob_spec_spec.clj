2290bd41ad0f225a439191f9999caa548a54ef62
Damon Kelley <damon.kelley@gmail.com>
2016-08-17 18:01:19 -0500

    Add the cob-spec application

---
+++ spec/http_clj/application/cob_spec_spec.clj
@@ -0,0 +1,39 @@
+(ns http-clj.application.cob-spec-spec
+  (:require [speclj.core :refer :all]
+            [clj-http.client :as client]
+            [http-clj.server :as s]
+            [http-clj.file :as file]
+            [http-clj.application.cob-spec :refer [app]])
+  (:import java.util.concurrent.CountDownLatch))
+
+(def root "http://localhost:5000")
+
+(defn new-latch []
+  (CountDownLatch. 1))
+
+(defn start-server [app port]
+  (let [latch (new-latch)
+        thread (Thread. #(s/serve (s/create port) app latch))]
+    (.start thread)
+    (.await latch)
+    thread))
+
+(defn shutdown-server [thread]
+  (.interrupt thread)
+  (client/get root))
+
+(def thread (start-server app 5000))
+
+(defn GET [path]
+  (client/get (str root path) {:throw-exceptions false}))
+
+(describe "cob-spec"
+  (after-all (shutdown-server thread))
+  (it "has /"
+    (let [{status :status body :body} (GET "/")]
+      (should= 200 status)
+      (should= "cob spec" body)))
+
+  (context "it can server images"
+    (it "has /image.gif"
+      (should= 200 (:status (GET "/image.gif"))))))
---
+++ src/http_clj/application/cob_spec.clj
@@ -0,0 +1,21 @@
+(ns http-clj.application.cob-spec
+  (:require [http-clj.router :refer [route]]
+            [http-clj.response :as response]
+            [http-clj.handler :as handler]
+            [http-clj.server :refer [run]]))
+
+(defn index [request]
+  (response/create request "cob spec"))
+
+(defn static [path]
+  (str "src/http_clj/application/static/" path))
+
+(defn app [request]
+  (route
+    request
+    {["GET" "/"] index
+     ["GET" "/image.gif"] (handler/file (static "image.gif"))}))
+
+(defn -main [& args]
+  (run app 5000))
+
