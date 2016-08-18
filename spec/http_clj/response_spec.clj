54870a8dd7ea08c58b611be5ce93ce1378e5ad46
Damon Kelley <damon.kelley@gmail.com>
2016-08-18 11:27:43 -0500

    Clean up the response specs

--- spec/http_clj/response_spec.clj
+++ spec/http_clj/response_spec.clj
@@ -2,9 +2,8 @@
   (:require [speclj.core :refer :all]
             [http-clj.response :refer [create compose]]
             [http-clj.spec-helper.mock :refer [connection]]
+            [http-clj.spec-helper.request-generator :refer [GET]]
             [clojure.string :as string]))
-
-(def request {:conn (connection)})
 
 (defn byte-array->string [array]
   (String. array))
@@ -17,47 +16,46 @@
 
   (context "when created"
     (it "has a body"
-      (should= "Hello, world!" (:body (create request "Hello, world!"))))
+      (should= "Hello, world!" (:body (create @request "Hello, world!"))))
 
     (it "has a status code"
-      (should= 200 (:status (create request "Body")))
-      (should= 404 (:status (create request "Body" :status 404))))
+      (should= 200 (:status (create @request "Body")))
+      (should= 404 (:status (create @request "Body" :status 404))))
 
     (it "has the connection"
-      (should= (:conn request) (:conn (create request "")))))
+      (should= (:conn @request) (:conn (create @request "")))))
 
   (context "when composed"
     (it "is a valid HTTP response"
-      (let [expected (str "HTTP/1.1 200 OK\r\n"
-                          "\r\n"
-                          "Hello, world!")]
-        (should= expected (-> (create request "Hello, world!")
-                              compose
-                              :message
-                              byte-array->string))))
+      (let [response (create @request "Hello, world!")
+            {message :message} (compose response)]
+        (should=
+          (str "HTTP/1.1 200 OK\r\n"
+               "\r\n"
+               "Hello, world!")
+          (byte-array->string message))))
 
     (it "uses the body from the response"
-      (should-contain "Message body" (-> (create request "Message body")
-                                         compose
-                                         :message
-                                         byte-array->string)))
+      (let [response (create @request "Message body")
+            {message :message} (compose response)]
+        (should-contain "Message body" (byte-array->string message))))
 
     (it "uses the body from the response"
-      (should-contain "Message body" (-> (create request (.getBytes "Message body"))
-                                         compose
-                                         :message
-                                         byte-array->string)))
+      (let [response (create @request (.getBytes "Message body"))
+            {message :message} (compose response)]
+        (should-contain "Message body" (byte-array->string message))))
 
     (it "uses the status from the response"
-      (should-contain "404" (-> (create request "" :status 404)
-                                compose
-                                :message
-                                byte-array->string)))
+      (let [response (create @request "" :status 404)
+            {message :message} (compose response)]
+        (should-contain "404" (byte-array->string message))))
 
     (it "has a status line for a GET request"
-      (let [{message :message} (compose (create @request "" :status 200))]
+      (let [response (create @request "" :status 200)
+            {message :message} (compose response)]
         (should= "HTTP/1.1 200 OK" (get-status-line message))))
 
     (it "has a status line for a GET request"
-      (let [{message :message} (compose (create @request "" :status 404))]
+      (let [response (create @request "" :status 404)
+            {message :message} (compose response)]
         (should= "HTTP/1.1 404 Not Found" (get-status-line message))))))
