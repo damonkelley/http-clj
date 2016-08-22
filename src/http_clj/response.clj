da942faf7dc7185ce0954d1a5c6d750eb0be5706
Damon Kelley <damon.kelley@gmail.com>
2016-08-22 11:17:06 -0500

    Add headers to responses

--- src/http_clj/response.clj
+++ src/http_clj/response.clj
@@ -1,9 +1,11 @@
 (ns http-clj.response
   (:require [clojure.string :as string]))
 
-(defn create [request body & {:keys [status]
-                              :or {status 200}}]
+(defn create [request body & {:keys [status headers]
+                              :or {status 200
+                                   headers {}}}]
   {:body body
+   :headers headers
    :status status
    :conn (:conn request)})
 
@@ -11,11 +13,19 @@
   {200 "OK"
    404 "Not Found"})
 
+(def CRLF "\r\n")
+
 (defn- reason-for [status]
   (get reasons status))
 
 (defn- to-byte-array [string]
   (byte-array (map (comp byte int) string)))
+
+(defn- format-header [[field-name field-value]]
+  (str field-name ": " field-value CRLF))
+
+(defn- format-headers [headers]
+  (apply str (map format-header headers)))
 
 (defn- generate-status-line [status]
   (let [version "HTTP/1.1"
@@ -23,14 +33,14 @@
         reason (reason-for status)]
     (str
       (string/join " " [version status reason])
-      "\r\n")))
+      CRLF)))
 
-(defn- generate-message [status body]
-  (->> [(generate-status-line status) "\r\n" body]
-      (map to-byte-array)
-      (mapcat seq)
-      (byte-array)))
+(defn- generate-message [status headers body]
+  (->> [(generate-status-line status) (format-headers headers) CRLF body]
+       (map to-byte-array)
+       (mapcat seq)
+       (byte-array)))
 
 (defn compose [resp]
-  (let [{status :status body :body} resp]
-    (assoc resp :message (generate-message status body))))
+  (let [{status :status headers :headers body :body} resp]
+    (assoc resp :message (generate-message status headers body))))
