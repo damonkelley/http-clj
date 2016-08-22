a648284621967dfb566f5535932fcdc66cc35d03
Damon Kelley <damon.kelley@gmail.com>
2016-08-22 15:31:08 -0500

    Add command line arguments

--- spec/http_clj/application/cob_spec_spec.clj
+++ spec/http_clj/application/cob_spec_spec.clj
@@ -40,6 +40,23 @@
     (it "has /image.gif"
       (should= 200 (:status (GET "/image.gif"))))))
 
+(describe "cli"
+  (it "accepts a valid port"
+    (should= 65535 (get-in (cli ["-p65535"]) [:options :port])))
+
+  (it "will fallback to the default port if the provided port is invalid"
+    (should-contain "Must be a number between 0 and 65536" (first (:errors (cli ["-p9000000"]))))
+    (should= 5000 (get-in (cli ["-p900000"]) [:options :port])))
+
+  (it "accepts a directory"
+    (should= "resources/static/" (get-in (cli ["-dresources/static/"]) [:options :directory])))
+
+  (it "defaults to public"
+    (should= "./public" (get-in (cli []) [:options :directory])))
+
+  (it "verifies the directory exists"
+    (should-contain "Must be a valid directory" (first (:errors (cli ["-ddoes-not-exist"]))))))
+
 (describe "fallback"
   (with dir "resources/static/")
   (it "responds with a listing if directory"
--- src/http_clj/application/cob_spec.clj
+++ src/http_clj/application/cob_spec.clj
@@ -1,7 +1,9 @@
 (ns http-clj.application.cob-spec
   (:require [http-clj.router :refer [route]]
             [http-clj.request-handler :as handler]
-            [http-clj.server :refer [run]])
+            [http-clj.server :refer [run]]
+            [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]])
   (:import java.nio.file.Paths))
 
 (defn file-helper [root-directory child-path]
@@ -21,5 +23,20 @@
 (defn app [directory]
   #(cob-spec % directory))
 
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 5000
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+
+   ["-d" "--directory DIR" "Directory to serve"
+    :default "./public"
+    :validate [#(.isDirectory (io/file %)) "Must be a valid directory"]]])
+
+(defn cli [args]
+  (-> args
+      (parse-opts cli-options)))
+
 (defn -main [& args]
-  (run (app "resources/static/") 5000))
+  (let [{options :options} (cli args)]
+    (run (app (:directory options)) (:port options))))
