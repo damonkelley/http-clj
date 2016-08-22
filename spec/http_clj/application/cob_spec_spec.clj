d1a32f45fe6d32169c2bc6a7e5b30555c365965b
Damon Kelley <damon.kelley@gmail.com>
2016-08-22 13:20:00 -0500

    Add a fallback handler
    The fallback handler will dispatch to a handler based on if the path is a
    valid path on disk.

--- spec/http_clj/application/cob_spec_spec.clj
+++ spec/http_clj/application/cob_spec_spec.clj
@@ -3,8 +3,11 @@
             [clj-http.client :as client]
             [http-clj.server :as s]
             [http-clj.file :as file]
-            [http-clj.application.cob-spec :refer [app]])
-  (:import java.util.concurrent.CountDownLatch))
+            [http-clj.request-handler :as handler]
+            [http-clj.application.cob-spec :refer :all]
+            [clojure.java.io :as io])
+  (:import java.util.concurrent.CountDownLatch
+           java.io.File))
 
 (def root "http://localhost:5000")
 
@@ -32,8 +35,27 @@
   (it "has /"
     (let [{status :status body :body} (GET "/")]
       (should= 200 status)
-      (should= "cob spec" body)))
+      (should-contain "file.txt" body)
+      (should-contain "image.gif" body)))
 
   (context "it can serve images"
     (it "has /image.gif"
       (should= 200 (:status (GET "/image.gif"))))))
+
+(describe "fallback"
+  (it "responds with a listing if directory"
+    (should-contain "image.gif" (:body (fallback {:path "/"}))))
+
+  (it "responds with a file if it is a file"
+    (should-contain "File contents" (String. (:body (fallback {:path "/file.txt"})))))
+
+  (it "responds with 404 if a file is not found"
+    (should= 404 (:status (fallback {:path "/file-that-does-not-exist.txt"})))))
+
+(describe "file-helper"
+  (it "takes two paths"
+    (file-helper "resources/static" "image.gif"))
+  (it "it forms the path correctly"
+    (should= "resources/static/image.gif" (.getPath (file-helper "resources/static/" "/image.gif"))))
+  (it "returns a file at the requsted path"
+    (should= "resources/static/image.gif" (.getPath (file-helper "resources/static" "image.gif")))))
--- src/http_clj/application/cob_spec.clj
+++ src/http_clj/application/cob_spec.clj
@@ -2,19 +2,31 @@
   (:require [http-clj.router :refer [route]]
             [http-clj.response :as response]
             [http-clj.request-handler :as handler]
-            [http-clj.server :refer [run]]))
+            [http-clj.server :refer [run]]
+            [clojure.java.io :as io])
+  (:import java.nio.file.Paths))
 
 (defn index [request]
   (response/create request "cob spec"))
 
 (defn static [path]
-  (str "src/http_clj/application/static/" path))
+  (str "resources/static/" path))
+
+(defn file-helper [root-directory child-path]
+  (-> (Paths/get root-directory (into-array [child-path]))
+      .toFile))
+
+(defn fallback [request]
+  (let [file (file-helper "resources/static/" (:path request))]
+    (cond (.isDirectory file) (handler/directory request file)
+          (.exists file) (handler/file request file)
+          :else (handler/not-found request))))
 
 (defn app [request]
   (route
     request
-    {["GET" "/"] index
-     ["GET" "/image.gif"] (handler/file (static "image.gif"))}))
+    {}
+    :fallback fallback))
 
 (defn -main [& args]
   (run app 5000))
