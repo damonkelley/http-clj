53331da798717139ecbda57dfed81281c19b7a97
Damon Kelley <damon.kelley@gmail.com>
2016-08-30 11:03:09 -0500

    Move router helpers into a new namespace

--- spec/http_clj/router_spec.clj
+++ spec/http_clj/router_spec.clj
@@ -1,6 +1,7 @@
 (ns http-clj.router-spec
   (:require [speclj.core :refer :all]
             [http-clj.router :refer :all]
+            [http-clj.router.route-helpers :refer [find-route]]
             [http-clj.request-handler :as handler]))
 
 (describe "router"
@@ -35,37 +36,6 @@
             {status :status body :body} (route request @routes)]
         (should-have-invoked :handler-z {:times 1}))))
 
-  (context "find-route"
-    (with routes [{:path "/a" :handlers {"GET" :handler-a}}
-                  {:path "/b" :handlers {"POST" :handler-b}}
-                  {:path #"^/pattern/.*$" :handlers {"GET" :handler-z}}])
-
-    (it "finds the route by path"
-      (should= {:path "/a" :handlers {"GET" :handler-a}}
-               (find-route @routes "/a")))
-
-    (it "is nil if there is no route for the path"
-      (should= nil (find-route @routes "/c")))
-
-    (it "can find a route using a pattern"
-      (should= {"GET" :handler-z} (:handlers (find-route @routes #"^/pattern/.*$")))))
-
-  (context "update-route"
-    (with routes [{:path "/a" :handlers {"GET" :handler-a}}
-                  {:path #"^/path/.*$" :handlers {}}])
-
-    (it "update the route if it exists"
-      (let [routes (update-route @routes {:path "/a" :handlers {"GET" :new-handler}})]
-        (should= {:path "/a" :handlers {"GET" :new-handler}} (find-route routes "/a"))))
-
-    (it "appends a new route if the path doesn't exist"
-      (let [routes (update-route @routes {:path "/b" :handlers {}})]
-        (should= {:path "/b" :handlers {}} (last routes))))
-
-    (it "can update a route with a pattern path"
-      (let [routes (update-route @routes {:path #"^/path/.*$" :handlers {"GET" :handler}})]
-        (should= {"GET" :handler}  (:handlers (find-route routes #"^/path/.*$"))))))
-
   (context "choose-handler"
     (with routes [{:path "/a" :handlers {"GET" :handler-get-a "POST" :handler-post-a}}
                   {:path "/b" :handlers {"HEAD" :handler-head-b "GET" :handler-get-b}}])
@@ -88,44 +58,44 @@
 
     (it "returns not-found if the route does not exist"
       (let [request {:method "DELETE" :path "/c"}]
-        (should= handler/not-found (choose-handler request @routes))))))
+        (should= handler/not-found (choose-handler request @routes)))))
 
-(describe "route helpers"
-  (context "GET"
-    (with routes [{:path "/a" :handlers {}}])
-    (it "adds the GET handler route to the route"
-      (let [route (-> @routes
-                      (GET "/b" :get-handler)
-                      (find-route "/b"))]
-        (should= :get-handler (get-in route [:handlers "GET"]))))
+  (context "routes functions"
+    (context "GET"
+      (with routes [{:path "/a" :handlers {}}])
+      (it "adds the GET handler route to the route"
+        (let [route (-> @routes
+                        (GET "/b" :get-handler)
+                        (find-route "/b"))]
+          (should= :get-handler (get-in route [:handlers "GET"]))))
 
-    (it "appends the route to the end of the routes vector"
-      (let [route (-> @routes
-                      (GET "/b" :get-handler)
-                      last)]
-      (should= "/b" (:path route))))
+      (it "appends the route to the end of the routes vector"
+        (let [route (-> @routes
+                        (GET "/b" :get-handler)
+                        last)]
+          (should= "/b" (:path route))))
 
-    (it "provides a default HEAD handler"
-      (let [route (-> @routes
-                       (GET "/b" :handler)
-                       (find-route "/b"))]
-        (should-be clojure.test/function? (get-in route [:handlers "HEAD"]))))
+      (it "provides a default HEAD handler"
+        (let [route (-> @routes
+                        (GET "/b" :handler)
+                        (find-route "/b"))]
+          (should-be clojure.test/function? (get-in route [:handlers "HEAD"]))))
 
-    (it "an alternate HEAD handler can be specified"
-      (let [route (-> @routes
-                       (GET "/b" :get-handler :head-handler)
-                       (find-route "/b"))]
-        (should= :head-handler (get-in route [:handlers "HEAD"]))))
+      (it "an alternate HEAD handler can be specified"
+        (let [route (-> @routes
+                        (GET "/b" :get-handler :head-handler)
+                        (find-route "/b"))]
+          (should= :head-handler (get-in route [:handlers "HEAD"]))))
 
-    (it "updates with a route if it is already defined"
-      (let [route (-> @routes
-                      (GET "/a" :handler)
-                      (find-route "/a"))]
-        (should= :handler (get-in route [:handlers "GET"]))))
+      (it "updates with a route if it is already defined"
+        (let [route (-> @routes
+                        (GET "/a" :handler)
+                        (find-route "/a"))]
+          (should= :handler (get-in route [:handlers "GET"]))))
 
-    (it "can update a route with a pattern"
-      (let [route (-> @routes
-                       (GET #"^/.*$" :first-handler)
-                       (GET #"^/.*$" :second-handler)
-                       (find-route #"^/.*$")) ]
-        (should= :second-handler (get-in route [:handlers "GET"]))))))
+      (it "can update a route with a pattern"
+        (let [route (-> @routes
+                        (GET #"^/.*$" :first-handler)
+                        (GET #"^/.*$" :second-handler)
+                        (find-route #"^/.*$")) ]
+          (should= :second-handler (get-in route [:handlers "GET"])))))))
