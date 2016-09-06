2ed333f0cdd39e1aed15856e45c2c758a7706336
Damon Kelley <damon.kelley@gmail.com>
2016-09-05 21:51:20 -0500

    Cleanup file spec

--- spec/http_clj/file_spec.clj
+++ spec/http_clj/file_spec.clj
@@ -3,23 +3,25 @@
             [http-clj.file :as file]))
 
 (describe "file"
+  (with test-path "/tmp/http-clj-test-file")
+  (before (spit @test-path ""))
+
   (context "binary-slurp"
-    (with test-path "/tmp/http-clj-test-file")
-    (before (spit @test-path ""))
+    (with contents "the quick brown fox")
+    (before (spit @test-path @contents))
 
     (it "reads the file contents into a byte-array"
-      (let [data "this quick brown fox"]
-        (spit @test-path data)
-        (should=
-          (seq (.getBytes data))
-          (seq (file/binary-slurp @test-path))))))
+      (should= (seq (.getBytes @contents))
+               (seq (file/binary-slurp @test-path)))))
 
   (context "file-helper"
     (it "takes two paths"
       (file/resolve "resources/static" "image.gif"))
 
     (it "it forms the path correctly"
-      (should= "resources/static/image.gif" (.getPath (file/resolve "resources/static/" "/image.gif"))))
+      (should= "resources/static/image.gif"
+               (.getPath (file/resolve "resources/static/" "/image.gif"))))
 
     (it "returns a file at the requsted path"
-      (should= "resources/static/image.gif" (.getPath (file/resolve "resources/static" "image.gif"))))))
+      (should= "resources/static/image.gif"
+               (.getPath (file/resolve "resources/static" "image.gif"))))))
