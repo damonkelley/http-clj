befc463a5cbe34d205869eedd0a176644bbeeefd
Damon Kelley <damon.kelley@gmail.com>
2016-09-07 11:54:32 -0500

    Parse and decode the path and query string

--- spec/http_clj/request/parser_spec.clj
+++ spec/http_clj/request/parser_spec.clj
@@ -10,13 +10,42 @@
   (tags "parser")
   (context "parse-request-line"
     (it "parses the request line into a map"
-      (should= {:method "GET" :path "/" :version "HTTP/1.1"}
+      (should= {:method "GET" :path "/" :version "HTTP/1.1" :query-params nil}
                (parser/parse-request-line "GET / HTTP/1.1"))
-      (should= {:method "POST" :path "/file" :version "HTTP/1.0"}
+      (should= {:method "POST" :path "/file" :version "HTTP/1.0" :query-params nil}
                (parser/parse-request-line "POST /file HTTP/1.0")))
 
-    (it "always has path version and method keys"
-      (should= {:method "" :path "" :version ""} (parser/parse-request-line ""))))
+    (it "parses the query parameters"
+      (let [request-line (parser/parse-request-line "GET /file?key=value HTTP/1.1")]
+        (should= {"key" "value"} (:query-params request-line))))
+
+    (it "strips the query string from the path"
+      (let [request-line (parser/parse-request-line "GET /file?key=value HTTP/1.1")]
+        (should= "/file" (:path request-line))))
+
+    (it "always has :path, :version :method, and :query-params"
+      (should= {:method "" :path "" :version "" :query-params nil}
+               (parser/parse-request-line ""))))
+
+  (context "parse-path"
+    (it "strips the query string from the path"
+      (let [request-line {:path "/file?key=value"}]
+        (should= "/file" (:path (parser/parse-path request-line)))))
+
+    (it "adds the query parameters to the map"
+      (let [request-line {:path "/file?key=value&parameter=other-value"}]
+        (should= {"key" "value" "parameter" "other-value"}
+                 (:query-params (parser/parse-path request-line)))))
+
+    (it "it decodes parameter fields"
+      (let [request-line {:path "/file?operators=%26%2C%3D%3B&sort=true"}]
+        (should= {"operators" "&,=;" "sort" "true"}
+                 (:query-params (parser/parse-path request-line)))))
+
+    (it ":query-params is nil if there is not query string"
+      (let [request-line {:method "GET" :path "/file" :version "HTTP/1.1"}]
+        (should= nil
+                 (:query-params (parser/parse-path request-line))))))
 
   (context "parse-headers"
     (it "returns an empty map if there are no headers"
--- src/http_clj/request/parser.clj
+++ src/http_clj/request/parser.clj
@@ -1,15 +1,49 @@
 (ns http-clj.request.parser
   (:require [http-clj.connection :as connection]
             [http-clj.request.reader :as reader]
-            [clojure.string :as string]))
+            [clojure.string :as string])
+  (:import java.net.URLDecoder))
 
 (defn- split-request-line [line]
-  (take 3 (concat (string/split line #" ") (repeat ""))))
+  (into [] (take 3 (concat (string/split line #" ") (repeat "")))))
+
+(defn- split-query-string [query-string]
+  (string/split query-string #"=|&"))
+
+(defn- -query-fields-to-map [query-fields]
+  (reduce
+    (fn [acc [key val]] (assoc acc key val))
+    {}
+    (partition 2 query-fields)))
+
+(defn- split-path-and-query-string [raw-path]
+  (zipmap
+    [:path :query-params]
+    (take 2 (concat (string/split raw-path #"\?") (repeat nil)))))
+
+(defn- decode-fields [query-fields]
+  (map #(URLDecoder/decode %) query-fields))
+
+(defn- parse-query-string [{:keys [query-params] :as parsed-path}]
+  (if query-params
+    (->> query-params
+         split-query-string
+         decode-fields
+         -query-fields-to-map
+         (assoc parsed-path :query-params))
+    parsed-path))
+
+(defn parse-path [request-line]
+  (->> (:path request-line)
+       split-path-and-query-string
+       parse-query-string
+       (merge request-line)))
 
 (defn parse-request-line [request-line]
   (->> request-line
        split-request-line
-       (zipmap [:method :path :version])))
+       (zipmap [:method :path :version])
+       parse-path))
 
 (defn- lower-case-field-name [[field-name field-value]]
   [(string/lower-case field-name) field-value])
