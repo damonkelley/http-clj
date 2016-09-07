aa3dc9b043c9e554e7f2c0ed72ae668e9b914eb3
Damon Kelley <damon.kelley@gmail.com>
2016-09-07 17:53:43 -0500

    Update the header parser to split the header on the first colon only

--- spec/http_clj/request/parser/headers_spec.clj
+++ spec/http_clj/request/parser/headers_spec.clj
@@ -11,6 +11,10 @@
       (should= {:host "www.example.com" :user-agent "Test-request"}
                (headers/parse ["Host: www.example.com" "User-Agent: Test-request"])))
 
+    (it "splits the field-name and field-value on the first colon"
+      (should= {:host "localhost:5000"}
+               (headers/parse ["Host: localhost:5000"])))
+
     (it "parses the header field values"
       (should= {:content-length 10}
                (headers/parse ["Content-Length: 10"]))))
