3d156ced0f6248d9163202815884c105d6555289
Damon Kelley <damon.kelley@gmail.com>
2016-09-07 15:49:38 -0500

    Use a temp file for the tag specs

--- spec/http_clj/entity_spec.clj
+++ spec/http_clj/entity_spec.clj
@@ -2,14 +2,11 @@
   (:require [speclj.core :refer :all]
             [http-clj.entity :as entity]
             [digest :refer [sha1]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import java.io.File))
 
 (describe "entity"
   (context "tag"
-    (with file "/tmp/http-clj-digest-file")
-    (with content "Test content")
-    (before (spit @file @content))
-
     (it "uses digest.sha1"
       (should-invoke sha1 {:times 1 :with ["content"]}
                      (entity/tag "content")))
@@ -18,8 +15,10 @@
       (should= "5e9b60f69165f32f8930843ca718e10fdee30c52" (entity/tag "tag"))
       (should= "040f06fd774092478d450774f5ba30c5da78acc8" (entity/tag "content")))
 
+    (with file (File/createTempFile "http-clj-digest" ".txt"))
+    (after (.delete @file))
+
     (it "accepts a File"
-      (let [file "/tmp/http-clj-digest-file"
-            content "Test content"]
-        (spit file content)
-        (should= (sha1 "Test content") (entity/tag (io/as-file file)))))))
+      (let [content "Test content"]
+        (spit (.getPath @file) content)
+        (should= (sha1 "Test content") (entity/tag @file))))))
