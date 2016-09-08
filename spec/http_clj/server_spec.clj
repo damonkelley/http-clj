107aa8e71b873cfbd2420365fca80191ae0bc7c6
Damon Kelley <damon.kelley@gmail.com>
2016-09-08 15:45:51 -0500

    Clean up the server specs

--- spec/http_clj/server_spec.clj
+++ spec/http_clj/server_spec.clj
@@ -10,43 +10,6 @@
   (:import java.io.ByteArrayOutputStream
            java.util.concurrent.Executors))
 
-(describe "a server component"
-  (with application {:entrypoint identity})
-
-  (it "can be created using a port"
-    (let [server (s/create @application
-                           :port 5001
-                           :server-socket mock/socket-server)]
-      (should-be-a java.net.ServerSocket (:server-socket server))
-      (component/stop server)))
-
-  (it "can be created by injecting a ServerSocket"
-    (let [server (s/create @application :server-socket mock/socket-server)]
-      (should-be-a java.net.ServerSocket (:server-socket server))))
-
-  (it "can be created with a thread pool"
-    (let [server (s/create @application
-                           :thread-pool :executor
-                           :server-socket mock/socket-server)]
-      (should= :executor (:thread-pool server))))
-
-  (it "has thread pool by default"
-    (let [server (s/create @application :server-socket mock/socket-server)]
-      (should-be-a java.util.concurrent.ExecutorService
-                   (:thread-pool server))))
-
-  (it "will close the server"
-    (let [server-socket (mock/socket-server)
-          server (s/create @application :server-socket mock/socket-server)]
-      (should= false (.isClosed (:server-socket server)))
-      (component/stop server)
-      (should= true (.isClosed (:server-socket server)))))
-
-  (it "will accept connections"
-    (let [conn (-> (s/create @application :server-socket mock/socket-server)
-                   s/accept)]
-      (should= true (satisfies? connection/Connection conn)))))
-
 (defrecord DegenerateLogger []
   logging/Logger
   (log [this level contents]))
@@ -56,41 +19,74 @@
     (should-fail "The connection should be open"))
   (response/create request "App was called"))
 
-(describe "process-request"
+(describe "server"
   (with application {:entrypoint test-app
                      :logger (->DegenerateLogger)})
 
-  (it "sends the conn and app to the http protocol"
-    (should-invoke protocol/http {:times 1 :return (mock/connection)}
-                   (s/process-request (mock/connection) @application)))
-  (it "closes the connection"
-    (let [conn (s/process-request (mock/connection) @application)]
-      (should= nil (:socket conn)))))
+  (describe "a server component"
+    (it "can be created using a port"
+      (let [server (s/create @application
+                             :port 5001
+                             :server-socket mock/socket-server)]
+        (should-be-a java.net.ServerSocket (:server-socket server))
+        (component/stop server)))
 
-(describe "listen"
-  (with server (mock/server))
-  (with application {:entrypoint test-app
-                     :logger (->DegenerateLogger)})
+    (it "can be created by injecting a ServerSocket"
+      (let [server (s/create @application :server-socket mock/socket-server)]
+        (should-be-a java.net.ServerSocket (:server-socket server))))
 
-  (it "creates a new worker to process the request"
-    (should-invoke s/new-worker {:times 1}
-                   (s/listen @server @application)))
-  )
+    (it "can be created with a thread pool"
+      (let [server (s/create @application
+                             :thread-pool :executor
+                             :server-socket mock/socket-server)]
+        (should= :executor (:thread-pool server))))
 
-(defn interrupting-app [request]
-  (loop [count 3]
-    (cond
-      (zero? count) (.interrupt (Thread/currentThread))
-      (neg? count) (should-fail "The loop should exit after interrupt")
-      :else (recur (dec count))))
-  (response/create request ""))
+    (it "has thread pool by default"
+      (let [server (s/create @application :server-socket mock/socket-server)]
+        (should-be-a java.util.concurrent.ExecutorService
+                     (:thread-pool server))))
 
-(describe "serve"
-  (with server (mock/server))
-  (with application {:entrypoint interrupting-app
-                     :logger (->DegenerateLogger)})
-  (it "starts the component"
-    (should= true (:started (s/serve @server))))
+    (it "will close the server"
+      (let [server-socket (mock/socket-server)
+            server (s/create @application :server-socket mock/socket-server)]
+        (should= false (.isClosed (:server-socket server)))
+        (component/stop server)
+        (should= true (.isClosed (:server-socket server)))))
 
-  (it "stops the component"
-    (should= true (:stopped (s/serve @server)))))
+    (it "will accept connections"
+      (let [conn (-> (s/create @application :server-socket mock/socket-server)
+                     s/accept)]
+        (should= true (satisfies? connection/Connection conn)))))
+
+  (describe "process-request"
+    (it "sends the conn and app to the http protocol"
+      (should-invoke protocol/http {:times 1 :return (mock/connection)}
+                     (s/process-request (mock/connection) @application)))
+    (it "closes the connection"
+      (let [conn (s/process-request (mock/connection) @application)]
+        (should= nil (:socket conn)))))
+
+  (describe "listen"
+    (with server (mock/server))
+
+    (it "creates a new worker to process the request"
+      (should-invoke s/new-worker {:times 1}
+                     (s/listen @server @application))))
+
+  (defn interrupting-app [request]
+    (loop [count 3]
+      (cond
+        (zero? count) (.interrupt (Thread/currentThread))
+        (neg? count) (should-fail "The loop should exit after interrupt")
+        :else (recur (dec count))))
+    (response/create request ""))
+
+  (describe "serve"
+    (with server (mock/server))
+    (with application {:entrypoint interrupting-app
+                       :logger (->DegenerateLogger)})
+    (it "starts the component"
+      (should= true (:started (s/serve @server))))
+
+    (it "stops the component"
+      (should= true (:stopped (s/serve @server))))))
