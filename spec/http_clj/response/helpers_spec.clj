b1b4dc16613d82d8cfb5487143d5aabb837bc650
Damon Kelley <damon.kelley@gmail.com>
2016-09-13 16:17:57 -0500

    Add a function to attach the content-range header

--- spec/http_clj/response/helpers_spec.clj
+++ spec/http_clj/response/helpers_spec.clj
@@ -3,10 +3,18 @@
             [http-clj.response.helpers :as helpers]))
 
 (describe "response.helpers"
-  (describe "assoc-content-type"
+  (describe "add-content-type"
     (it "adds the content-type to the response"
       (should= {:headers {:content-type "image/gif"}}
                (helpers/add-content-type {} "image.gif")))
 
     (it "does not add the header if the mimetype can not be determined"
-      (should= {} (helpers/add-content-type {} "file")))))
+      (should= {} (helpers/add-content-type {} "file"))))
+
+  (describe "add-content-range"
+    (it "adds the content-range to the response"
+      (should= {:headers {:content-range "bytes 0-3/77"}}
+               (helpers/add-content-range {} "bytes" 0 3 77))
+
+      (should= {:headers {:content-range "chars 6-10/100"}}
+               (helpers/add-content-range {} "chars" 6 10 100)))))
--- src/http_clj/response/helpers.clj
+++ src/http_clj/response/helpers.clj
@@ -5,3 +5,10 @@
   (if-let [content-type (file/content-type-of path)]
     (assoc-in resp [:headers :content-type] content-type)
     resp))
+
+(defn- format-content-range [units start end length]
+  (str units " " start "-" end "/" length))
+
+(defn add-content-range [resp units start end length]
+  (assoc-in resp [:headers :content-range]
+            (format-content-range units start end length)))
